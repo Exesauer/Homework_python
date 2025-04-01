e272d12dca0ab450fc2f36567be51641ba4d07ae
Exesauer <sacred112rus@gmail.com>
2025-04-01 22:22:41 +0300

    First commit for lesson_01

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,22 @@
+def print_letter(num):
+    print(num)
+    
+print_letter('8')
+print_letter('8')
+print_letter('8')
+print_letter('0')
+print_letter('0')
+print_letter('5')
+print_letter('5')
+print_letter('5')
+print_letter('3')
+print_letter('5')
+print_letter('3')
+print_letter('5')
+
+# def print_letter(num):
+#     print(num)
+
+# Вызов функции для печати цифр
+# for digit in '88005553535':
+#     print_letter(digit)
